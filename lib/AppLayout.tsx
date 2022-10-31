242417bcea6be4a4ae335b16751ff2de722ed6c0
ripaelit <ripaelit1111@gmail.com>
2022-10-31 20:21:47 +0600

    Fix - Layout style

--- lib/AppLayout.tsx
+++ lib/AppLayout.tsx
@@ -12,16 +12,16 @@ export default function AppLayout({ children }: { children: JSX.Element }) {
         .container {
           display: flex;
           flex-direction: column;
-          min-width: 100vw;
+
           align-items: center;
           min-height: 100%;
-          padding: 0 30px;
         }
 
         .app-container {
           width: 100%;
           max-width: 1280px;
           height: 100%;
+          padding: 0 30px;
         }
       `}</style>
     </>
