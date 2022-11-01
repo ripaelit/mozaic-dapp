8c7bf86deca6edd912c10653754386053750ef83
ripaelit <ripaelit1111@gmail.com>
2022-11-01 19:23:29 +0600

    fix - responsiveness

--- lib/components/common/menu/DesktopMenu.tsx
+++ lib/components/common/menu/DesktopMenu.tsx
@@ -1,45 +1,22 @@
 import React from 'react';
 import OutsideClickHandler from 'react-outside-click-handler';
 import { menuItems } from '../../../data/static/nav';
+import MenuBox from './MenuBox';
 
 export default function DesktopMenu({ showMenu, setShowMenu }: any) {
   return (
     <>
-      <div className='arrow'></div>
-      <div className='menu-container'>
-        {menuItems.map((item) => (
-          <div onClick={() => setShowMenu(false)} key={item.id} className='menu-item-wrapper'>
-            <img src={item.icon} alt='' />
-            <p>{item.name}</p>
-          </div>
-        ))}
-      </div>
+      <MenuBox>
+        <>
+          {menuItems.map((item) => (
+            <div onClick={() => setShowMenu(false)} key={item.id} className='menu-item-wrapper'>
+              <img src={item.icon} alt='' />
+              <p>{item.name}</p>
+            </div>
+          ))}
+        </>
+      </MenuBox>
       <style jsx>{`
-        .arrow {
-          width: 20px;
-          height: 20px;
-          border-radius: 8px 0 0 0;
-          position: absolute;
-          top: 54px;
-          right: 12px;
-          transform: rotate(45deg);
-          background-color: var(--cardBgPrimary);
-        }
-        .menu-container {
-          position: absolute;
-          top: 64px;
-          right: -10px;
-          display: flex;
-          flex-direction: column;
-          width: 280px;
-          padding: 8px;
-          box-shadow: 0px 40px 64px -12px rgba(0, 0, 0, 0.08), 0px 0px 14px -4px rgba(0, 0, 0, 0.05),
-            0px 32px 48px -8px rgba(0, 0, 0, 0.1);
-          backdrop-filter: blur(16px);
-          border-radius: 16px;
-          background-color: var(--cardBgPrimary);
-        }
-
         .menu-item-wrapper {
           width: 100%;
           padding: 10px 16px;
