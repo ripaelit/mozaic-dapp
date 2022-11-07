1c41a1ab7f98a91b444865592200aab73f91a3af
ripaelit <ripaelit1111@gmail.com>
2022-11-07 10:15:35 +0600

    add - product in depth static data

---
+++ lib/data/static/productInDepth.ts
@@ -0,0 +1,14 @@
+export const knowledgeBaseData = [
+  {
+    id: 0,
+    title: 'Fee Structure',
+    description:
+      'The vault fee consists of a 1% withdrawal fee and a 10% performance fee. The performance fee is charged based on the assets managed by the vault. If the strategy is profitable, the performance fee is charged on the premiums when withdrawing; however, if the strategy is unprofitable, there are no fees charged.',
+  },
+  {
+    id: 1,
+    title: 'Vault Strategy',
+    description:
+      'The vault gives users exposure to a specific category of tokens. Every 15 minutes the vault calculates if it should re-balance the portfolio according to our modelling system and then rotates the tokens to the highest rewarding protocol to farm with.',
+  },
+];
