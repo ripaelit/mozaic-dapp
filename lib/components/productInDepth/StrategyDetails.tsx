a6dc26674cca4feeafa071a679f9b35b7ebb837a
ripaelit <ripaelit1111@gmail.com>
2022-11-09 23:01:32 +0600

    add - chart on strategy details section

--- lib/components/productInDepth/StrategyDetails.tsx
+++ lib/components/productInDepth/StrategyDetails.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { generateChart } from '../../data/dummy/randomChartData';
+import { sortByTimelineTabItems } from '../../data/static/productInDepth';
+import FilterTab from '../common/tab/FilterTab';
+import Chart from './charts/Chart';
 
-export default function StrategyDetails() {
+const series = [
+  { name: 'USDC', id: 0, data: generateChart(1500, 4) },
+  { name: 'USDT', id: 1, data: generateChart(1500, 4) },
+  { name: 'BUSD', id: 1, data: generateChart(1500, 4) },
+];
+
+export default function StrategyDetails({ style }: any) {
+  const [timeline, setTimeline] = useState(sortByTimelineTabItems[0]);
+
   return (
     <>
-      <div className='strategy-details-container'></div>
-      <style jsx>{``}</style>
+      <div className='strategy-details-container'>
+        <div className='product-chart-container'>
+          <div className='product-chart-options-wrapper'>
+            <FilterTab
+              tabs={sortByTimelineTabItems}
+              currentTab={timeline}
+              setCurrentTab={setTimeline}
+            />
+          </div>
+          <Chart timeline={timeline.value} series={series} />
+        </div>
+      </div>
+      <style jsx>{`
+        .strategy-details-container {
+          width: 100%;
+          ${style ? style : ''}
+        }
+      `}</style>
     </>
   );
 }
