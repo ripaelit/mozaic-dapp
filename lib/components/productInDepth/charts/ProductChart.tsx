9d71e1e11100d595847b6dc60b2d60676ce0c160
ripaelit <ripaelit1111@gmail.com>
2022-11-09 23:02:48 +0600

    fix - product chart style

--- lib/components/productInDepth/charts/ProductChart.tsx
+++ lib/components/productInDepth/charts/ProductChart.tsx
@@ -1,29 +1,39 @@
 import dynamic from 'next/dynamic';
 import React, { useState } from 'react';
+import { generateChart } from '../../../data/dummy/randomChartData';
 import { sortByTimelineTabItems } from '../../../data/static/productInDepth';
 import PrimaryCard from '../../common/card/PrimaryCard';
 import FilterTab from '../../common/tab/FilterTab';
-// import Chart from './Chart';
 const Chart = dynamic(() => import('./Chart'), {
   ssr: false,
 });
 
-export default function ProductChart() {
+const series = [{ name: 'Yo Bro', id: 0, data: generateChart(1500, 4) }];
+
+export default function ProductChart({ style }: { style?: string }) {
   const [timeline, setTimeline] = useState(sortByTimelineTabItems[0]);
   return (
     <>
-      <PrimaryCard title={{ visible: false }}>
-        <div className='product-chart-container'>
-          <div className='product-chart-options-wrapper'>
-            <FilterTab
-              tabs={sortByTimelineTabItems}
-              currentTab={timeline}
-              setCurrentTab={setTimeline}
-            />
+      <div className='product-chart-container'>
+        <PrimaryCard title={{ visible: false }} style={` padding: 24px;`}>
+          <div className='product-chart-container'>
+            <div className='product-chart-options-wrapper'>
+              <FilterTab
+                tabs={sortByTimelineTabItems}
+                currentTab={timeline}
+                setCurrentTab={setTimeline}
+              />
+            </div>
+            <Chart timeline={timeline.value} series={series} />
           </div>
-          <Chart timeline={timeline.value} />
-        </div>
-      </PrimaryCard>
+        </PrimaryCard>
+      </div>
+      <style jsx>{`
+        .product-chart-container {
+          width: 100%;
+          ${style ? style : ''}
+        }
+      `}</style>
     </>
   );
 }
