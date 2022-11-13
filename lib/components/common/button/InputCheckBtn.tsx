67fac099027bcafe5a1b02c3353e2d52d72991bf
ripaelit <ripaelit1111@gmail.com>
2022-11-13 20:01:33 +0600

    add - max logic & other check button

---
+++ lib/components/common/button/InputCheckBtn.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import useBalance from '../../../hooks/useBalance';
+
+// get max balance of the asset
+export default function InputCheckBtn({
+  type,
+  text,
+  active,
+  onClick,
+  onMax,
+  currentAsset,
+  currentDepositData,
+}: {
+  type?: string;
+  text?: string;
+  active?: boolean;
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
+  onMax?: Function;
+  currentAsset: any;
+  currentDepositData?: any;
+}) {
+  const [maxBalance] = useBalance(currentAsset.address, currentAsset.decimals);
+  return (
+    <>
+      <div
+        className={`max-btn-container ${
+          type === 'max' ? (maxBalance == currentDepositData.amount ? 'active' : '') : active
+        }`}
+        onClick={
+          type !== 'max'
+            ? onClick
+            : () => {
+                onMax!(maxBalance);
+              }
+        }>
+        {type === 'max' ? 'Max' : text}
+      </div>
+      <style jsx>{`
+        .max-btn-container {
+          padding: 10px;
+          background-color: var(--inputBtnBgPrimary);
+          cursor: pointer;
+          color: var(--textSecondary);
+          user-select: none;
+          border-radius: 8px;
+        }
+        .active {
+          color: var(--primary);
+          background-color: var(--primaryT1;
+        }
+      `}</style>
+    </>
+  );
+}
