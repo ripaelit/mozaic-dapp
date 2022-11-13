365893a818e50f3a81b8d179c9fbfb37e89673be
ripaelit <ripaelit1111@gmail.com>
2022-11-13 20:02:35 +0600

    add - text with icon dropdown

---
+++ lib/components/common/input/dropdown/DropdownWithIcon.tsx
@@ -0,0 +1,152 @@
+import React, { useState } from 'react';
+import OutsideClickHandler from 'react-outside-click-handler';
+
+type Option = {
+  id: any;
+  name: string;
+  value: string;
+};
+
+export default function DropdownWithIcon({
+  options,
+  setSelectedOption,
+  selectedOption,
+}: {
+  options: Option[];
+  setSelectedOption: React.Dispatch<React.SetStateAction<Option>>;
+  selectedOption: Option;
+}) {
+  const [openDropdown, setOpenDropdown] = useState(false);
+  return (
+    <>
+      <div className='dropdown-container'>
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            setOpenDropdown(false);
+          }}>
+          <div
+            className={`dropdown-wrapper ${openDropdown && 'opened'}`}
+            onClick={() => {
+              setOpenDropdown(!openDropdown);
+            }}>
+            <div className='dropdown-selected-item'>
+              <img src={selectedOption.icon} alt='' />
+              <p>{selectedOption.name}</p>
+            </div>
+            <img
+              className={`dropdown-icon ${openDropdown ? 'flipped' : ''}`}
+              src='/assets/icons/ico.dropdown.svg'
+              alt=''
+            />
+          </div>
+          {/* on dropdown open */}
+          {openDropdown && (
+            <div className='dropdown-options-wrapper'>
+              {options.map((option: Option) => (
+                <div
+                  key={option.id}
+                  className='dropdown-option'
+                  onClick={() => {
+                    setSelectedOption(option);
+                    setOpenDropdown(!openDropdown);
+                  }}>
+                  <img src={option.icon} alt='' />
+                  <p>{option.name}</p>
+                </div>
+              ))}
+            </div>
+          )}
+        </OutsideClickHandler>
+      </div>
+      <style jsx>{`
+        .dropdown-container {
+          position: relative;
+          z-index: 1;
+        }
+
+        .dropdown-wrapper {
+          display: flex;
+          width: 100%;
+          justify-content: space-between;
+          align-items: center;
+          width: 150px;
+          max-width: max-content;
+          padding: 8px 8px 8px 8px;
+          background-color: var(--inputBtnBgPrimary);
+          gap: 4px;
+          border-radius: 8px;
+          cursor: pointer;
+          font-size: 0.875rem;
+          user-select: none;
+          transition: all 0.2s ease;
+        }
+
+        .dropdown-wrapper.opened {
+          background-color: var(--outlinePrimary);
+        }
+
+        .dropdown-wrapper:hover {
+          background-color: var(--outlinePrimary);
+        }
+
+        .dropdown-selected-item {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          gap: 8px;
+        }
+
+        .dropdown-selected-item > img {
+          width: 28px;
+          height: 28px;
+        }
+
+        .dropdown-icon {
+          transition: all 0.2s ease;
+        }
+
+        .dropdown-icon.flipped {
+          transform: rotate(180deg);
+        }
+
+        .dropdown-options-wrapper {
+          top: calc(100% + 8px);
+          right: 0;
+          position: absolute;
+          display: flex;
+          flex-direction: column;
+          gap: 8px;
+          padding: 8px;
+          min-width: 100%;
+          font-size: 0.875rem;
+          background-color: var(--bg);
+          border-radius: 8px;
+          box-shadow: var(--shadowPrimary);
+        }
+
+        .dropdown-option {
+          padding: 4px 8px;
+          cursor: pointer;
+          border-radius: 4px;
+          transition: all 0.2s ease;
+          display: flex;
+          align-items: center;
+          gap: 8px;
+        }
+        .dropdown-option:hover {
+          background-color: var(--textPrimaryT1);
+        }
+        .dropdown-option > img {
+          width: 28px;
+          height: 28px;
+        }
+
+        @media screen and (max-width: 450px) {
+          .dropdown-selected-item > p {
+            display: none;
+          }
+        }
+      `}</style>
+    </>
+  );
+}
