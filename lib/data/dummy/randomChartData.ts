a462f6b464975ef3e082b317b0cf5befa18abe32
ripaelit <ripaelit1111@gmail.com>
2022-12-09 07:02:43 +0600

    fix - chart stacking & tooltip display

--- lib/data/dummy/randomChartData.ts
+++ lib/data/dummy/randomChartData.ts
@@ -11,3 +11,10 @@ export const generateChart = (count: number, interval: number, startFrom?: numbe
 
   return arr;
 };
+
+const arr = generateChart(300, 3);
+
+const arr2 = arr.map((data: any) => [
+  data[0],
+  parseInt(Math.round(Math.random() * 100).toFixed(2)),
+]);
