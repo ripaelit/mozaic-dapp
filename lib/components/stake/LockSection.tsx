91ec82a46a1e5d6e74f35b720f75e272f086361e
ripaelit <ripaelit1111@gmail.com>
2022-12-20 18:44:17 +0600

    add - dynamic slider

--- lib/components/stake/LockSection.tsx
+++ lib/components/stake/LockSection.tsx
@@ -1,3 +1,4 @@
+import ReactSlider from 'react-slider';
 import InputCheckBtn from '../common/button/InputCheckBtn';
 import PrimaryBtn from '../common/button/PrimaryBtn';
 import PrimaryCard from '../common/card/PrimaryCard';
@@ -47,6 +48,7 @@ export default function LockSection({
             />
             <InputWithLabel
               datePicker={true}
+              inputType={'week'}
               label='Choose lock time'
               inputValue={time}
               onChange={(e: any) => {
@@ -62,12 +64,72 @@ export default function LockSection({
             />
           </div>
           <div className='bottom'>
-            <Slider max={156} min={0} current={selectedRange.value} />
-            <p className='info'>Your starting voting power will be 0.00 veMOZ!</p>
+            <ReactSlider
+              min={1}
+              max={156}
+              className='horizontal-slider'
+              thumbClassName='lock-slider-thumb'
+              trackClassName='lock-slider-track'
+              value={time}
+              onChange={setTime}
+              renderThumb={(props, state) => (
+                <div {...props} className='handle'>
+                  <div className='inner'>{state.valueNow}</div>
+                </div>
+              )}
+            />
+            <p className='info'>
+              Your starting voting power will be {!amount ? 0 : amount} veMOZ over {time}{' '}
+              {time > 1 ? 'weeks' : 'week'}!
+            </p>
             <PrimaryBtn text='Create lock' onClick={onLock} />
           </div>
         </div>
       </PrimaryCard>
+      <style jsx global>{`
+        .horizontal-slider {
+          width: 100%;
+          height: 30px;
+          display: flex;
+          align-items: center;
+        }
+        .lock-slider-track-0 {
+          height: 4px;
+          background-color: var(--primary);
+        }
+        .lock-slider-track-1 {
+          height: 4px;
+          background-color: var(--outlinePrimary);
+        }
+        .handle:focus-visible {
+          outline: var(--primary);
+        }
+        .handle {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          height: 24px;
+          width: 24px;
+          border-radius: 12px;
+          background-color: var(--primary);
+          user-select: none;
+          cursor: pointer;
+        }
+        .inner {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          text-align: center;
+          width: 75%;
+          height: 75%;
+          border-radius: 50%;
+          color: var(--primaryDark);
+          font-size: 0.675rem;
+          font-weight: bold;
+          background-color: var(--textPrimary);
+          box-shadow: 0 3px 4px rgba(0, 0, 0, 0.34);
+        }
+      `}</style>
       <style jsx>{`
         .lock-wrapper {
           display: flex;
