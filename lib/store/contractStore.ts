64ce1fabd150cdc03a1c48ff330bea58342424e9
ripaelit <ripaelit1111@gmail.com>
2023-03-21 20:14:18 +0800

    Add utility function for SecondaryVault

--- lib/store/contractStore.ts
+++ lib/store/contractStore.ts
@@ -1,8 +1,17 @@
 import ERC20ABI from './assets/abi-erc20.json';
+import SecondaryVaultABI from './assets/abi-SecondaryVault.json';
 
 export function getERC20Contract(tokenAddress: any, web3: any) {
   return web3
     ? new web3.eth.Contract(ERC20ABI, tokenAddress, {
+        from: web3.eth.defaultAccount,
+      })
+    : null;
+}
+
+export function getSecondaryVaultContract(tokenAddress: any, web3: any) {
+  return web3
+    ? new web3.eth.Contract(SecondaryVaultABI, tokenAddress, {
         from: web3.eth.defaultAccount,
       })
     : null;
