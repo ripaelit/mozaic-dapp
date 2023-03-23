dfbf5b1ab689bf22689b935fcfc90e29d03a5312
ripaelit <ripaelit1111@gmail.com>
2023-03-23 17:27:13 +0800

    add getWeb3 util function.

--- lib/store/contractStore.ts
+++ lib/store/contractStore.ts
@@ -1,5 +1,17 @@
 import ERC20ABI from './assets/abi-erc20.json';
 import SecondaryVaultABI from './assets/abi-SecondaryVault.json';
+import Web3 from 'web3';
+import { chains } from '../data/static/wallet';
+
+export function getWeb3(chainName: string) {
+  const chain = chains.find(obj => obj.name == chainName)
+  if (!chain) {
+    return null;
+  }
+  const web3Provider = new Web3.providers.HttpProvider(chain!.rpcUrls);
+  const web3 = new Web3(web3Provider);
+  return web3;
+}
 
 export function getERC20Contract(tokenAddress: any, web3: any) {
   return web3
